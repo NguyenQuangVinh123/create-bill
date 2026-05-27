01133428306fcf2c69cfc3e3d146da28fe330648
vinhnq-devblock <vinhnq@devblock.net>
2026-05-27 12:01:24 +0700

    add speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin"] });
 
 
@@ -16,6 +17,7 @@ export default function RootLayout({
         {children}
       </body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
