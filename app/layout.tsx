ba9d870eb7a4702e2f30993e9f80888e909ef655
vinhnq-devblock <vinhnq@devblock.net>
2026-05-27 11:51:17 +0700

    add analics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 const inter = Inter({ subsets: ["latin"] });
 
 
@@ -15,6 +15,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
       </body>
+      <Analytics />
     </html>
   );
 }
